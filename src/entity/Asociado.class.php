361fc66ce4e30d6bc2a09000746462e4aa695a22
Eric Lasso <eric.lasso1698@gmail.com>
2025-10-27 19:07:45 +0100

    v0.10.2

---
+++ src/entity/Asociado.class.php
@@ -0,0 +1,72 @@
+<?php
+class Asociado {
+    /**
+    *@var string
+    */
+    private $id;
+    /**
+    *@var string
+    */
+    private $nombre;
+    /**
+    *@var string
+    */
+    private $logo;
+    /**
+    *@var string
+    */
+    private $descripcion;
+    const RUTA_LOGOS_ASOCIADOS = __DIR__ . '/../../public/images/asociados/';
+
+    public function __construct($nombre, $logo, $descripcion) {
+        $this->id = null;
+        $this->nombre = $nombre;
+        $this->logo = $logo;
+        $this->descripcion = $descripcion;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+	public function getNombre() {
+        return $this->nombre;
+    }
+
+	public function getLogo() {
+        return $this->logo;
+    }
+
+	public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+    public function setId( $id): Asociado {
+        $this->id = $id;
+        return $this;
+    }
+
+	public function setNombre( $nombre): Asociado {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+	public function setLogo( $logo): Asociado {
+        $this->logo = $logo;
+        return $this;
+    }
+
+	public function setDescripcion( $descripcion): Asociado {
+        $this->descripcion = $descripcion;
+        return $this;
+    }
+
+	public function getUrl(): string {
+        return self::RUTA_LOGOS_ASOCIADOS . $this->logo;
+    }
+
+	function __tostring(): string {
+        return $this->descripcion;
+    }
+	
+}
