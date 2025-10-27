a749f08c6863afab2918197c93208724c6b7d017
Eric Lasso <eric.lasso1698@gmail.com>
2025-10-27 20:29:51 +0100

    v0.10.3

--- templates/Asociados.php
+++ templates/Asociados.php
@@ -5,7 +5,8 @@ $errores = [];
 $mensaje = "";
 $nombre = "";
 $descripcion = "";
-
+$captchaInput = "";
+session_start();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         $nombre = trim(htmlspecialchars($_POST['nombre']));
@@ -14,16 +15,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (empty($nombre)) {
             $errores[] = "El nombre del asociado es obligatorio.";
         }
+        if (isset($_POST['captcha']) && ($_POST['captcha'] != "")) {
+            if ($_SESSION['captchaGenerado'] != $_POST['captcha']) {
+                $mensaje = "¡Ha introducido un código de seguridad incorrecto! Inténtelo de nuevo.";
+                $errores = [];
+                $nombre = "";
+                $descripcion = "";
+            } else {
+                if (empty($errores)) {
+                    $tiposAceptados = ["image/jpg", "image/png", "image/gif"];
 
-        if (empty($errores)) {
-            $tiposAceptados = ["image/jpg", "image/png", "image/gif"];
+                    $logo = new File('logo', $tiposAceptados);
+                    $logo->saveUploadFile(Asociado::RUTA_LOGOS_ASOCIADOS);
 
-            $logo = new File('logo', $tiposAceptados);
-            $logo->saveUploadFile(Asociado::RUTA_LOGOS_ASOCIADOS);
+                    $asociado = new Asociado($nombre, $logo->getFileName(), $descripcion);
 
-            $asociado = new Asociado($nombre, $logo->getFileName(), $descripcion);
-
-            $mensaje = "El asociado '{$asociado->getNombre()}' se ha registrado correctamente.";
+                    $mensaje = "El asociado '{$asociado->getNombre()}' se ha registrado correctamente.";
+                }
+            }
+        } else {
+            $mensaje = "";
+            $errores[] = "Introduzca el código de seguridad.";
+            $nombre = "";
+            $descripcion = "";
         }
     } catch (FileException $err) {
         $errores[] = $err->getMessage();
