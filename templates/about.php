2ddf94808d78a77cde1534bd32a1147010a60ab6
Eric Lasso <eric.lasso1698@gmail.com>
2025-11-10 22:44:33 +0100

    v0.11.6-corregido2

--- templates/about.php
+++ templates/about.php
@@ -1,6 +1,6 @@
 <?php 
 require_once __DIR__ . '/../src/entity/imagen.class.php';
-$imagenesClientes= [new Imagen('client1.jpg','MISS BELLA', 'cliente'),
+$imagenesClientes= [new Imagen('client1.jpg','MISS BELLA'),
 new Imagen('client2.jpg','DON PENO', 0),
 new Imagen('client3.jpg','SWEETY', 0),
 new Imagen('client4.jpg','LADY', 0)];
