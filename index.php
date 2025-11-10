c3767c1b816e0daeb5ad7dfa8163878527bdefb4
Eric Lasso <eric.lasso1698@gmail.com>
2025-11-11 00:09:13 +0100

    v0.13.2

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-require_once 'core/bootstrap.php';
-$routes = require 'app/routes.php'; // Obtenemos la tabla de rutas
-$uri = trim($_SERVER['REQUEST_URI'], '/'); // Obtenemos la uri del usuario
-require $routes[$uri];
-exit();
+try {
+    require_once 'core/bootstrap.php';
+    require Router::load('app/routes.php')->direct(Request::uri());
+    }
+    catch ( NotFoundException $notFoundException)
+    {
+    die($notFoundException->getMessage());
+    }
