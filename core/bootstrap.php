50f1f640199ef1061eaf2f48460fdd4bc71c29fd
Eric Lasso <eric.lasso1698@gmail.com>
2025-11-11 16:23:08 +0100

    v0.14.1

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,9 +1,12 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/utils/MyLog.php';
 
 require_once __DIR__ . '/App.php';
 require_once __DIR__ . '/Request.php';
 require_once __DIR__ . '/Router.php';
 require_once __DIR__ . '/../src/exceptions/NotFoundException.class.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $config = require_once __DIR__ . '/../app/config.php';
 
@@ -12,3 +15,6 @@ App::bind('config', $config);
 
 $router = Router::load('app/routes.php');
 App::bind('router',$router);
+
+$logger = MyLog::load('logs/curso.log');
+App::bind('logger',$logger);
